e39c5aab56ce67f56d2e9e5a47d776aaeff771fb
Liam Chalcroft <liam.chalcroft.20@ucl.ac.uk>
2023-12-15 08:40:23 -0600

    ++

--- hypunet/training/network_training/HyperTrainerV2.py
+++ hypunet/training/network_training/HyperTrainerV2.py
@@ -164,9 +164,8 @@ class HyperTrainerV2(HyperTrainer):
 
             self.initialize_network()
             self.initialize_optimizer_and_scheduler()
-            print(type(self.network))
 
-            assert isinstance(self.network, (SegmentationNetwork, nn.DataParallel))
+            # assert isinstance(self.network, (SegmentationNetwork, nn.DataParallel))
         else:
             self.print_to_log_file(
                 "self.was_initialized is True, not running self.initialize again"
